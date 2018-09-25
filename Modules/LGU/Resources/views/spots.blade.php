8383fc53e8e4f544d39ae9970cc13e24fa64139c
gabbydgab <gamba@gabbydgab.com>
2018-09-25 15:21:04 +0800

    updated page

--- Modules/LGU/Resources/views/spots.blade.php
+++ Modules/LGU/Resources/views/spots.blade.php
@@ -1,7 +1,37 @@
 @extends('dashboard::layouts.master');
 @section('content')
 @include('lgu::partial.mobile-header')
-@include('lgu::partial.sidebar')
+<!-- MENU SIDEBAR-->
+<aside class="menu-sidebar d-none d-lg-block">
+    <div class="logo">
+        <a href="#">
+            <img src="images/icon/logo.png" alt="Cool Admin" />
+        </a>
+    </div>
+    <div class="menu-sidebar__content js-scrollbar1">
+        <nav class="navbar-sidebar">
+            <ul class="list-unstyled navbar__list">
+                <li>
+                    <a class="js-arrow" href="{{route('lgu.home')}}">
+                        <i class="fas fa-tachometer-alt"></i>Dashboard</a>
+                </li>
+                <li class="active">
+                    <a href="{{route('lgu.spots')}}">
+                        <i class="fas fa-chart-bar"></i>Tourist Spots</a>
+                </li>
+                <li>
+                    <a href="{{route('lgu.restos')}}">
+                        <i class="fas fa-home"></i>Restaurants</a>
+                </li>
+                <li>
+                    <a href="#">
+                        <i class="fas fa-calendar-alt"></i>Events</a>
+                </li>
+            </ul>
+        </nav>
+    </div>
+</aside>
+<!-- END MENU SIDEBAR-->
 <div class="page-container">
     @include('lgu::partial.desktop-header')
     <div class="main-content">
